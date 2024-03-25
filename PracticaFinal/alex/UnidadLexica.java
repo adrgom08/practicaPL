e63bfa61bcc9e96729a22b434a57122233d968f0
Adrián Gómez Lamuedra <adrigomail@gmail.com>
2024-03-25 02:46:34 +0100

    El .cup en teoria reconoce expresiones, voy a preguntar a ver como compilarlo bien para ir añadiendo mas cosas que reconozca

--- PracticaFinal/alex/UnidadLexica.java
+++ PracticaFinal/alex/UnidadLexica.java
@@ -1,5 +1,7 @@
 package alex;
 
+import java_cup.runtime.Symbol;
+
 public class UnidadLexica extends Symbol {
     public UnidadLexica(int fila, int columna, int clase) {
 	super(clase,new TokenValue(fila,columna));
--- PracticaFinal/asint/Main.java
+++ PracticaFinal/asint/Main.java
@@ -1,26 +1,15 @@
 package asint;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
-
 import alex.AnalizadorLexicoTiny;
-import alex.ClaseLexica;
-import alex.UnidadLexica;
 
 public class Main {
-   public static void main(String[] args) throws FileNotFoundException, IOException {
+   public static void main(String[] args) throws Exception {
      Reader input = new InputStreamReader(new FileInputStream(args[0]));
-     AnalizadorLexicoTiny al = new AnalizadorLexicoTiny(input);
-     UnidadLexica unidad;
-
-     do {
-       unidad = al.yylex();
-       System.out.println(unidad);
-     }
-
-     while (unidad.clase() != ClaseLexica.EOF);
-    }
-} 
+	 AnalizadorLexicoTiny alex = new AnalizadorLexicoTiny(input);
+	 AnalizadorSintacticoTiny asint = new AnalizadorSintacticoTiny(alex);
+	 asint.parse();
+ }
+}   
